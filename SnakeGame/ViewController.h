cd98b65584fb20d9433cf935bb43a3550a3b5ba8
Syngmaster <max6361@mail.ru>
2017-04-20 18:55:08 +0100

    Initial Commit

---
+++ SnakeGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnakeGame
+//
+//  Created by Syngmaster on 20/04/2017.
+//  Copyright © 2017 Syngmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
