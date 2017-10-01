38c8deb45d81e23abe34f8ae2856d151ad926839
Syngmaster <max6361@mail.ru>
2017-10-01 16:52:43 +0100

    Ready to implement interstitial ad

--- SnakeGame/SMCustomAlertViewController.m
+++ SnakeGame/SMCustomAlertViewController.m
@@ -8,6 +8,7 @@
 
 #import "SMCustomAlertViewController.h"
 #import "SMSnakeEngineModel.h"
+#import <Social/Social.h>
 
 @interface SMCustomAlertViewController ()
 
@@ -51,8 +52,29 @@
 
 - (IBAction)facebookShareAction:(UIButton *)sender {
     
+    if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]) {
+        
+        SLComposeViewController *facebookVC = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+        
+        [facebookVC setInitialText:@"I'm playing Advanture of Snake!"];
+        [facebookVC addImage:[UIImage imageNamed:@"main_logo.png"]];
+        
+        [self presentViewController:facebookVC animated:YES completion:nil];
+        
+    } else {
+        
+        [self showAlertMessage:@"Please sign in Facebook"];
+    }
     
+}
+
+
+- (void)showAlertMessage:(NSString *)myMessage{
     
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"SocialShare" message:myMessage preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:nil]];
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
 
